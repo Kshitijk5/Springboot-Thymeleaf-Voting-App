dcb5aba78bba25a9dc4e521c5b95ed7802638757
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-02 19:30:49 +0530

    Add User.java

---
+++ src/main/java/com/voting/Votingapp/model/User.java
@@ -0,0 +1,38 @@
+package com.voting.Votingapp.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String  username;
+
+    private String password;
+    private String email;
+    private Boolean voted;
+    private String profilePic;
+    private String profileUrl;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id")
+    )
+    private Set<Role> roles;
+
+    @ManyToOne
+    private Party party;
+
+
+
+
+}
