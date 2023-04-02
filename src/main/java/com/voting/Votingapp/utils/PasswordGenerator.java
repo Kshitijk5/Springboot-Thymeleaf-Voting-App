1099e8dcd605f9c029f5ea5ef22029a2ed4a131c
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-02 19:20:28 +0530

    Add utils for password generation

---
+++ src/main/java/com/voting/Votingapp/utils/PasswordGenerator.java
@@ -0,0 +1,12 @@
+package com.voting.Votingapp.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+        System.out.println(  passwordEncoder.encode("123"));
+    }
+}
