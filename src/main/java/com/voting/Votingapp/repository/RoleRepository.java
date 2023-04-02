d05b15dbb31981bc88f2d3aff642abea3d49b83f
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-02 19:29:46 +0530

    Add Role.java Repository

---
+++ src/main/java/com/voting/Votingapp/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.voting.Votingapp.repository;
+
+import com.voting.Votingapp.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
+}
