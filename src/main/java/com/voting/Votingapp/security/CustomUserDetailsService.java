769a6dc6538c92140ab87bd33665073be1b53925
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-02 19:27:31 +0530

    Add setting profile url to CustomUserDetail instance

---
+++ src/main/java/com/voting/Votingapp/security/CustomUserDetailsService.java
@@ -0,0 +1,35 @@
+package com.voting.Votingapp.security;
+
+import com.voting.Votingapp.repository.UserRepository;
+import com.voting.Votingapp.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String usernameOrEmail) throws UsernameNotFoundException {
+        User user = userRepository.findByUsernameOrEmail(usernameOrEmail, usernameOrEmail)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with username or email: "+ usernameOrEmail));
+
+        Set<GrantedAuthority> authorities = user
+                .getRoles()
+                .stream()
+                .map((role) -> new SimpleGrantedAuthority(role.getName())).collect(Collectors.toSet());
+
+     return new CustomUserDetails(user.getUsername(),user.getEmail(),user.getPassword(),user.getProfileUrl(),authorities);
+    }
+
+}
