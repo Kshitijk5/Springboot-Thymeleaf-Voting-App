dfdb18b4fe0e00737f26d4a22926533965545cc5
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-02 19:50:35 +0530

    Add byName and byId handlers

--- src/main/java/com/voting/Votingapp/repository/PartyRepository.java
+++ src/main/java/com/voting/Votingapp/repository/PartyRepository.java
@@ -4,6 +4,11 @@ package com.voting.Votingapp.repository;
 import com.voting.Votingapp.model.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
 public interface PartyRepository extends JpaRepository<Party,Long> {
 
+    Optional<Party> findById(long id);
+    Optional<Party> findByPartyName(String partName);
 }
