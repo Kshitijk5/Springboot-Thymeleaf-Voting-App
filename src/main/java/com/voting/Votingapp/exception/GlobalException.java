f19d5aa5271a8c92d3fd0525dc99a1e8e77592bc
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-03 17:40:49 +0530

    MaxFileuploadException fix

--- src/main/java/com/voting/Votingapp/exception/GlobalException.java
+++ src/main/java/com/voting/Votingapp/exception/GlobalException.java
@@ -1,10 +1,13 @@
 package com.voting.Votingapp.exception;
 
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalException {
@@ -12,8 +15,8 @@ public class GlobalException {
 
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
-    public String imageMaxSize(MaxUploadSizeExceededException ex){
-        System.out.println(ex);
+    public String imageMaxSize(MaxUploadSizeExceededException ex, Model model){
+        model.addAttribute("errormsg","Add image less than 5MB, password reset failed.");
         return "Error-Page";
     }
     @ExceptionHandler(MultipartException.class)
@@ -29,9 +32,15 @@ public class GlobalException {
         return "access-denied";
     }
 
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String noHandler(NoHandlerFoundException ex){
+        System.out.println(ex);
+        return "access-denied";
+    }
     @ExceptionHandler(Exception.class)
     public String globalErrorHandler(Exception ex){
         System.out.println(ex);
+
         return "Error-Page";
     }
 }
