9d0c8dac8983bf6fda0f61b0776dc178f2c94ed6
Kshitij <kshitij.kanojiyarockz@gmail.com>
2023-04-03 14:19:58 +0530

    Add AppConstants

---
+++ src/main/java/com/voting/Votingapp/utils/AppConstants.java
@@ -0,0 +1,17 @@
+package com.voting.Votingapp.utils;
+
+public class AppConstants {
+
+    //Auth-controller
+    public static final String REGISTER_STATUS = "status";
+    public static final String  REGISTER_USERNAME_EXISTS="username";
+    public static final String  REGISTER_EMAIL_EXISTS="email";
+    public static final boolean  REGISTER_SUCCESS=true;
+
+
+    //Default pfp
+
+    public static final String DEFAULT_PFP="man.png";
+
+
+}
